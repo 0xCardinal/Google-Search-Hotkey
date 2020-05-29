c7d45ef8afb3f901232cbc728998f97f581bc5d0
Kumar Ashwin <krashwin00@gmail.com>
2020-05-29 12:37:33 +0530

    Opens in new tab

--- search.py
+++ search.py
@@ -1,15 +1,12 @@
+#! /bin/python3
+
 # Automation using Python : Searching internet (Google) using simple keyboard shortcut.
 # Author : Kumar Ashwin, krashwin00@gmail.com
-# Version: 1.0, 15th Jan, 2020
+# Version: 2.0, 29th May, 2020
 
-import pyperclip
-from selenium import webdriver
-
-try:
-    browser = webdriver.Firefox() #geckodriver(firefox) is available in the environment path
-except:
-    print("Firefox's driver is only specifed! Specify the driver of your choice of web browser.")
+import pyperclip, webbrowser
 
 query = pyperclip.paste().replace(" ","+") #taking the last copied text and making it URL ready
 search_url = "https://www.google.com/search?q="+query
-browser.get(search_url)
+
+webbrowser.open_new_tab(search_url)
