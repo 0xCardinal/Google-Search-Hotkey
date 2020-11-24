76170f6a7603f83d8a91a0efc2da222c4639bf5d
krAshwin <krashwin00@gmail.com>
2020-11-25 02:15:14 +0530

    Updates code base - shifts to go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/krAshwin/Google-Search-Hotkey
+
+go 1.15
+
+require github.com/atotto/clipboard v0.1.2
---
+++ search.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	"github.com/atotto/clipboard"
+)
+
+func openbrowser(url string) {
+	var err error
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		fmt.Println("Unsupported Plaform!")
+		os.Exit(1)
+	}
+}
+
+func search(url string) {
+	if strings.HasPrefix(url, "https://") || strings.HasPrefix(url, "http://") {
+		openbrowser(url)
+	} else {
+		url = "https://www.google.com/search?q=" + url
+		openbrowser(url)
+	}
+}
+
+func main() {
+
+	if len(os.Args) == 1 {
+		url, err := clipboard.ReadAll()
+		if err != nil {
+			log.Fatal(err)
+		}
+		search(url)
+	} else {
+		param := os.Args[1]
+		search(param)
+	}
+}
